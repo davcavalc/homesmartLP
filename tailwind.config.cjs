b1bdcd4469b188b630a04da5607c4959725ddd49
Davner Firmino Cavalcante <davner23@gmail.com>
2022-07-17 17:48:02 -0400

    Criação da navbar (parte 1) - 17/07/2022

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+ content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+ theme: {
+  extend: {},
+ },
+ plugins: [
+  require("@tailwindcss/forms"),
+  // ...
+ ],
+};
